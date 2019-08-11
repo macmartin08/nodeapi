0a9b8bc47bfd728b35d22b20b6e5ba0ec6ceb46f
ravi <asodariya>
2019-08-11 18:32:59 -0400

    change

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
  
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://reactproject1.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
